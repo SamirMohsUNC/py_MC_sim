b5bbc8118c1a1ffa3cb0c27cc5e3b35b3319a3e0
SamirMohsUNC <155934611+SamirMohsUNC@users.noreply.github.com>
2025-08-06 01:05:01 +0200

    main.py integration with optimizer

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import numpy as np
 import pandas as pd 
 import yfinance as yf
 from datetime import datetime, timedelta
+
 from src.simulate import simulate_portfolio_losses
 from src.risk_metrics import compute_var, compute_cvar
+from src.optimizer import minimize_cvar
 
+
+# Create questions for user to answer
 def get_user_input():
     tickers = input("Enter stock tickers separated by commas (e.g. AAPL,MSFT,GOOGL): ").strip().upper().split(',')
     tickers = [t.strip() for t in tickers]
@@ -43,25 +47,48 @@ def prepare_inputs(data, investments):
     return mu, sigma, weights
 
 
+def display_portfolio(tickers, weights, total_value, title="Portfolio Allocation"):
+    print(f"\n{title}:")
+    for t, w in zip(tickers, weights):
+        print(f"{t}: {w:.2%} -> ${w * total_value:.2f}")
+
+
+# Now to run user logic
 def main():
-    print("Interactive Monte Carlo Portfolio Risk Simulation")
+    print("Interactive Monte Carlo Portfolio Risk Simulation With Optimization")
 
     tickers, investments, window_days, horizon = get_user_input()
     print(f"\nFetching data for {tickers}...")
     data = fetch_data(tickers, window_days)
 
     mu, sigma, weights = prepare_inputs(data, investments)
+    total_value = np.sum(investments)
 
-    print(f"\nRunning Monte Carlo simulation for a {horizon}-day horizon...")
-    losses = simulate_portfolio_losses(mu, sigma, weights, T=horizon, N=10000)
+    print(f"\nRunning Monte Carlo simulation for a {horizon}-day investment horizon...")
+    losses = simulate_portfolio_losses(mu, sigma, weights, T=horizon, N=100000)
 
     var_95 = compute_var(losses, 0.95)
     cvar_95 = compute_cvar(losses, 0.95)
 
-    total_value = np.sum(investments)
+    display_portfolio(tickers, weights, total_value, "Initial Portfolio Allocation)")
 
     print(f"\n Portfolio VaR 95% = {var_95:.4%} ({var_95 * total_value:.2f} USD)")
     print(f"\n Portfolio CVaR 95% = {cvar_95:.4%} ({cvar_95 * total_value:.2f} USD)")
+
+    choice = input("\nWould you like to optimize your portfolio to minimize CVaR? (y/n): ").strip().lower()
+    if choice == 'y':
+        print("\nOptimizing portfolio...")
+        opt_weights = minimize_cvar(mu, sigma, T=horizon)
+
+        opt_losses = simulate_portfolio_losses(mu, sigma, opt_weights, T=horizon, N=100000)
+        opt_var = compute_var(opt_losses, 0.95)
+        opt_cvar = compute_cvar(opt_losses, 0.95)
+
+        display_portfolio(tickers, opt_weights, total_value, "Optimized Portfolio Allocation")
+        print(f"\nOptimized VaR 95% ={opt_var:.4%} ({opt_var * total_value:.2f} USD)")
+        print(f"\nOptimized CVaR 95% ={opt_cvar:.4%} ({opt_cvar * total_value:.2f} USD)")
+    else:
+        print("\nNo optimization performed.")
 
 
 if __name__ == "__main__":
--- src/optimizer.py
+++ src/optimizer.py
@@ -1,3 +1,39 @@
+import numpy as np
 from scipy.optimize import minimize
 from src.simulate  import simulate_portfolio_losses
 from src.risk_metrics import compute_cvar
+
+def minimize_cvar(mu, sigma, T, N=100000):
+    d = len(mu)
+
+    def objective(w):
+        losses = simulate_portfolio_losses(mu, sigma, w, T=T, N=N)
+        return compute_cvar(losses, 0.95)
+    
+    
+    constraints = [{'type': 'eq', 'fun':lambda w: np.sum(w) - 1}]
+
+    # Set dynamic constraints based on portfolio size
+    if d<= 5:
+        max_weight = 0.50
+    elif d==6:
+        max_weight = 0.45
+    elif d==7:
+        max_weight = 0.40
+    elif d==8:
+        max_weight = 0.35
+    elif d==9:
+        max_weight = 0.30
+    elif d>=10:
+        max_weight = 0.25
+
+
+    bounds = [(0.0, max_weight)] * d
+    initial_guess = [1/d] * d
+
+    result = minimize(objective, initial_guess, method='SLSQP', bounds=bounds, constraints=constraints)
+
+    if not result.success:
+        raise RuntimeError("Optimization failed:", result.message)
+    
+    return result.x
