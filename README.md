8dbca6cd71eb4ec4464b25fa61e3b616738a51bd
SamirMohsUNC <155934611+SamirMohsUNC@users.noreply.github.com>
2025-08-06 19:37:22 +0200

    README and optimizer.py edits

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ The program will then optimize the given portfolio to minimize Value at Risk and
 
 All methodology is clearly explained in src and test files, with optional stress testing available. 
 
-Since these simulations are minimizing VaR and CVaR, this is meant for short term trading rather than holding stocks long term.
+Since these simulations are minimizing VaR and CVaR, this is meant for short term trading rather than holding stocks long term. Trading is also meant to be performed with stocks, rather than ETFs or mutual funds. Since stocks should only make up 5-15% of an entire portfolio, this does not put additional risk on the optimization constraints on quantity of a stock.
 
-Of course, the market has inherent risks and this should not be used for real investing. 
+Of course, the market has inherent risks and this should not be used for serious investing. 
--- src/optimizer.py
+++ src/optimizer.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize
-from src.simulate  import simulate_portfolio_losses
-from src.risk_metrics import compute_cvar
+from simulate import simulate_portfolio_losses
+from risk_metrics import compute_cvar
 
 def minimize_cvar(mu, sigma, T, N=100000):
     d = len(mu)
@@ -10,22 +10,22 @@ def minimize_cvar(mu, sigma, T, N=100000):
         losses = simulate_portfolio_losses(mu, sigma, w, T=T, N=N)
         return compute_cvar(losses, 0.95)
     
-    
+
     constraints = [{'type': 'eq', 'fun':lambda w: np.sum(w) - 1}]
 
     # Set dynamic constraints based on portfolio size
     if d<= 5:
-        max_weight = 0.50
-    elif d==6:
-        max_weight = 0.45
-    elif d==7:
-        max_weight = 0.40
-    elif d==8:
         max_weight = 0.35
+    elif d==6:
+        max_weight = 0.32
+    elif d==7:
+        max_weight = 0.29
+    elif d==8:
+        max_weight = 0.26
     elif d==9:
-        max_weight = 0.30
+        max_weight = 0.23
     elif d>=10:
-        max_weight = 0.25
+        max_weight = 0.20
 
 
     bounds = [(0.0, max_weight)] * d
